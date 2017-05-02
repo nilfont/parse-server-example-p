c2225801f29ebdcf11d89da6e1369877f61de407
nilfont <nilfont@users.noreply.github.com>
2017-05-02 10:42:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'acampadajoveJKSN8HD9151SJlaskSYUEkasJdsAS',
+  masterKey: process.env.MASTER_KEY || 'AhjabhjhJSJKSUnnsJUehKSacampada9128ijoveJSAKnsm', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://acampadajove.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    classNames: ["Time"]// List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
